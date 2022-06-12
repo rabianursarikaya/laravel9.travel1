00251773425452a4aaffb0979ce7f97c237601c7
rabianusarikaya <sarikayarabianur@gmail.com>
2022-06-12 13:10:23 +0300

    committed
    show data category done

---
+++ app/Models/Review.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'place_id',
+        'user_id',
+        'IP',
+        'subject',
+        'review',
+    ];
+
+    public function place(){
+        return $this->belongsTo(Place::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
